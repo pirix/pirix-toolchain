1c8d96b1050e3dfb98116194e47de119a4bc9a59
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2012-11-24 07:10:24 +0100

    fix

--- data/newlib/crt0.s
+++ data/newlib/crt0.s
@@ -1,13 +1,9 @@
 .arm
 .extern main
-.extern exit
+.extern call_exit
 .global _start
 
 _start:
-    ldr r0, [r13, #4]
-    ldr r1, [r13, #8]
-    push {r1, r0}
     bl main
-    add r14, r14, #8
-    bl exit
-    bl .
+    bl call_exit
+    b .
--- data/newlib/syscalls.h
+++ data/newlib/syscalls.h
@@ -1,21 +1,32 @@
-#define SYS_REBOOT   0x0000
-#define SYS_LOG      0x0001
+#pragma once
 
-#define SYS_EXIT     0x1000
-#define SYS_KILL     0x1001
-#define SYS_WAIT     0x1002
-#define SYS_FORK     0x1003
-#define SYS_GETPID   0x1004
-#define SYS_ISATTY   0x1005
-#define SYS_EXECVE   0x1006
+// System
+#define SYS_REBOOT   0x00
+#define SYS_LOG      0x01
 
-#define SYS_LINK     0x2000
-#define SYS_UNLINK   0x2001
+// Processes
+#define SYS_EXIT     0x10
+#define SYS_KILL     0x11
+#define SYS_WAIT     0x12
+#define SYS_FORK     0x13
+#define SYS_GETPID   0x14
+#define SYS_ISATTY   0x15
+#define SYS_EXECVE   0x16
 
-#define SYS_CLOSE    0x4000
-#define SYS_OPEN     0x4001
-#define SYS_READ     0x4002
-#define SYS_WRITE    0x4003
-#define SYS_LSEEK    0x4004
-#define SYS_STAT     0x4005
-#define SYS_FSTAT    0x4006
+// IPC
+#define SYS_SEND     0x30
+#define SYS_RECV     0x31
+
+// Filesystem
+#define SYS_CLOSE    0x40
+#define SYS_OPEN     0x41
+#define SYS_READ     0x42
+#define SYS_WRITE    0x43
+#define SYS_LSEEK    0x44
+#define SYS_STAT     0x45
+#define SYS_FSTAT    0x46
+#define SYS_LINK     0x47
+#define SYS_UNLINK   0x48
+
+// Memory
+#define SYS_SBRK     0x60
